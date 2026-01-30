e74f262c244dd8e7d5104d85facfe6c5a8a6884a
FluffyKafka <2304705734@qq.com>
2026-01-30 17:44:01 +0800

    合并动画组件

--- Assets/Refactor/Script/ObjectAnimationAndFx/MObjectAnimationFx.cs
+++ Assets/Refactor/Script/ObjectAnimationAndFx/MObjectAnimationFx.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace ObjectAnimationFx
+namespace AnimationAndFx
 {
 
     internal class MObjectAnimationFx : ComponentManagerBase, IObjectAnim
